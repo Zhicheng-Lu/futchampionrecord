9654546875ed8e155d9836fcc6a9445767eb2a53
andylu.zc@gmail.com <andylu.zc@gmail.com>
2019-11-04 15:56:26 +1100

    small bugs

--- new_fut_champion_tabs/squad/squad.php
+++ new_fut_champion_tabs/squad/squad.php
@@ -6,7 +6,7 @@
 				<?php
 				for ($i=1; $i < 12; $i++) { 
 					echo '
-				<div id="position_'.$i.'" class="col-xxl-24 col-xl-30 col-lg-40 col-sm-60" style="border: 1px solid #888888; border-radius: 5px; height: 150px; cursor: pointer;" onclick="open_modal('.$i.')">';
+				<div id="position_'.$i.'" class="col-xxl-24 col-xl-30 col-lg-40 col-sm-60" style="border: 1px solid #888888; border-radius: 5px; height: 150px; cursor: pointer;" onclick="open_modal('.$i.')" draggable="true">';
 
 					$sql = 'SELECT * FROM squad AS S LEFT JOIN players AS P ON S.player_id=P.id WHERE S.position='.$i;
 					$result = $conn->query($sql);
@@ -14,7 +14,7 @@
 						echo '
 					<div class="row" style="height: 100%;">
 						<div style="height: 100%;" class="col-50">
-							<img src="images/cards/'.$row["id"].'.png" style="height: 100%;">
+							<img src="images/cards/'.$row["id"].'.png" style="height: 100%;" draggable="false">
 						</div>
 						<div style="height: 100%;" class="col-70">
 							<span onclick="clear_player('.$i.')" style="float: right; font-size: 16px;"><b>&times;</b></span>
@@ -36,7 +36,7 @@
 				<?php
 				for ($i=12; $i < 19; $i++) { 
 					echo '
-				<div id="position_'.$i.'" class="col-xxl-24 col-xl-30 col-lg-40 col-sm-60" style="border: 1px solid #888888; border-radius: 5px; height: 150px; cursor: pointer;" onclick="open_modal('.$i.')">';
+				<div id="position_'.$i.'" class="col-xxl-24 col-xl-30 col-lg-40 col-sm-60" style="border: 1px solid #888888; border-radius: 5px; height: 150px; cursor: pointer;" onclick="open_modal('.$i.')" draggable="true">';
 
 					$sql = 'SELECT * FROM squad AS S LEFT JOIN players AS P ON S.player_id=P.id WHERE S.position='.$i;
 					$result = $conn->query($sql);
@@ -44,7 +44,7 @@
 						echo '
 					<div class="row" style="height: 100%;">
 						<div style="height: 100%;" class="col-50">
-							<img src="images/cards/'.$row["id"].'.png" style="height: 100%;">
+							<img src="images/cards/'.$row["id"].'.png" style="height: 100%;" draggable="false">
 						</div>
 						<div style="height: 100%;" class="col-70">
 							<span onclick="clear_player('.$i.')" style="float: right; font-size: 16px;"><b>&times;</b></span>
